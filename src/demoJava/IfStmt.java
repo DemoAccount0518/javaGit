151feeafa5a6176976577670c62075ce1ed370e9
DemoAccount0518 <bunatamil@gmail.com>
2019-05-01 14:04:17 -0700

    Modified IfStmt

--- src/demoJava/IfStmt.java
+++ src/demoJava/IfStmt.java
@@ -5,7 +5,9 @@ public class IfStmt {
 	public static void main(String[] args) {
 		int num=50;
 		if(num<100){
-		System.out.println("Number is >than 100");
+		System.out.println("Number is < than 100");
+		System.out.println("I am adding code to my program");
+		
 		}
 	}
 }
